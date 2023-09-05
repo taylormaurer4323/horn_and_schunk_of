3397425a64947a4f6d4e51bc4cdf3a010efbc8ce
taylormaurer4323 <taylormaurer4323@gmail.com>
2023-09-04 19:06:09 -0600

    gradient calc, just using opencv

--- include/project/gradient.h
+++ include/project/gradient.h
@@ -1,26 +1,18 @@
 #include <iostream>
-#include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
-#include <opencv2/core/eigen.hpp>
 
 #ifndef GRADIENT_H_
 #define GRADIENT_H_
 
-using namespace Eigen;
 
 namespace img_util 
 {
+
   void gradient_x(const cv::Mat & input, cv::Mat & output);
   void gradient_y(const cv::Mat & input, cv::Mat & output);
   void gradient(const cv::Mat & input, cv::Mat & output);
-  void shift_gradient_calc(int s_r, int s_c, 
-    const Array<float, Dynamic, Dynamic> & img,
-    Array<float, Dynamic, Dynamic> & grad_img);
-
-  void sobel_gradient_calc(
-    const Array<float, Dynamic, Dynamic> & img,
-    Array<float, Dynamic, Dynamic> & grad_img);
-
+  void get_avg_img(const cv::Mat & img, cv::Mat & avg_img);
+  void shift_img(int s_r, int s_c, const cv::Mat & img, cv::Mat & shifted_img_diff);
 }
 
 
--- src/gradient.cpp
+++ src/gradient.cpp
@@ -1,15 +1,65 @@
 #include "project/gradient.h"
-#include "project/preprocess_util.h"
-#include <Eigen/Dense>
+#include <cmath>
 #include <opencv2/opencv.hpp>
-#include <opencv2/core/eigen.hpp>
 
-using namespace Eigen;
-/*
+
+
+void img_util::gradient_x(const cv::Mat & input, cv::Mat & output)
+{
+    cv::GaussianBlur(input, output, cv::Size(5,5), 0);
+    cv::Sobel(output, output, CV_32F, 1, 0, 5);
+    
+}
+
+void img_util::gradient_y(const cv::Mat & input, cv::Mat & output)
+{
+    cv::GaussianBlur(input, output, cv::Size(5,5), 0);
+    cv::Sobel(input, output, CV_32F, 0, 1, 5);
+    
+}
+
+void img_util::gradient(const cv::Mat & input, cv::Mat & output)
+{
+    cv::Laplacian(input, output, CV_32F);
+}
+void img_util::get_avg_img(const cv::Mat & img, cv::Mat & avg_img)
+{
+    cv::Mat top_img(avg_img.rows, avg_img.cols, CV_32F);
+    cv::Mat right_img(avg_img.rows, avg_img.cols, CV_32F);
+    cv::Mat bottom_img(avg_img.rows, avg_img.cols, CV_32F);
+    cv::Mat left_img(avg_img.rows, avg_img.cols, CV_32F);
+    img_util::shift_img(0, -1, img, top_img);
+    img_util::shift_img(1, 0, img, right_img);
+    img_util::shift_img(0, 1, img, bottom_img);
+    img_util::shift_img(-1, 0, img, left_img);
+    avg_img = 0.25 * (top_img + right_img + bottom_img + left_img);
+    
+
+
+
+}
+void img_util::shift_img(int s_c, int s_r,
+    const cv::Mat & img, cv::Mat & shifted_img)
+{    
+    int og_c = std::max(0, s_c); 
+    int og_r = std::max(0, s_r);
+    int new_c = abs(std::min(0, s_c));
+    int new_r = abs(std::min(0, s_r));
+    //std::cout<<"Indexing from ("<<og_c<<", "<<og_r<<") w/width ("<<img.cols - new_c - og_c<<", "<<img.rows-new_r - og_r<<")\n";
+    cv::Rect r(og_c, og_r, img.cols-new_c - og_c, img.rows-new_r - og_r);
+    
+    //std::cout<<"Indexing from ("<<new_c<<", "<<new_r<<") w/width ("<<img.cols - new_c - og_c<<", "<<img.rows-new_r - og_r<<")\n";    
+    cv::Rect r_ind(new_c, new_r, img.cols - new_c - og_c, img.rows - new_r - og_r);
+    shifted_img = cv::Mat::zeros(shifted_img.rows, shifted_img.cols, CV_32F);
+    img(r).copyTo(shifted_img(r_ind));
+    
+}
+/* Below is an old implementation
 * Input:
 *   s_r: shift in rows [pixels]
 *   s_c: shift in columns [pixels]
 */
+/*
 void img_util::shift_gradient_calc(int s_r, int s_c, 
     const Array<float, Dynamic, Dynamic> & img, 
     Array<float, Dynamic, Dynamic> & grad_img)
@@ -23,6 +73,7 @@ void img_util::shift_gradient_calc(int s_r, int s_c,
     //Now subtract the two matrices
     grad_img = img_redux - img_shift;
 }
+
 
 void img_util::gradient_x(const cv::Mat & input, cv::Mat & output)
 {
@@ -64,3 +115,4 @@ void img_util::gradient(const cv::Mat & input, cv::Mat & output)
     pp_util::array2cv(grad_img_eig, output);
 }
 
+*/
